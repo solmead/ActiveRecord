fed94fe194bfe007609a98598d620ffde881644c
solmead <solmead@gmail.com>
2013-04-06 21:34:13 -0400

    Added notes for each function

--- ActiveRecord/CodeFirst/Record.vb
+++ ActiveRecord/CodeFirst/Record.vb
@@ -95,6 +95,12 @@ Namespace CodeFirst
         Private Shared Function Convert(ByVal entity As TT) As Record(Of TT)
             Return CType(CType(entity, Object), Record(Of TT))
         End Function
+        ''' <summary>
+        ''' Get Queryable entry for current type
+        ''' </summary>
+        ''' <param name="db">Data Context</param>
+        ''' <returns></returns>
+        ''' <remarks></remarks>
         Public Shared Function GetList(ByVal db As DbContext) As IQueryable(Of TT)
             If db IsNot Nothing Then
                 Dim l = From I In db.Set(GetType(TT)).AsQueryable() Select CType(I, TT)
@@ -104,6 +110,14 @@ Namespace CodeFirst
                 Return l.AsQueryable
             End If
         End Function
+        ''' <summary>
+        ''' Gets requested item, uses find on DbSet
+        ''' </summary>
+        ''' <param name="db">Code First Data context</param>
+        ''' <param name="id">Id of item to find</param>
+        ''' <param name="returnNothing">If item is not found return null or nothing</param>
+        ''' <returns>Returns item found, by default if item is not found returns empty item</returns>
+        ''' <remarks></remarks>
         Public Shared Function Load(ByVal db As DbContext, ByVal id As Integer, Optional returnNothing As Boolean = False) As TT
             Dim tp As Type = GetType(TT)
             If db IsNot Nothing Then
@@ -128,6 +142,11 @@ Namespace CodeFirst
                 Return Nothing
             End If
         End Function
+        ''' <summary>
+        ''' Marks that this item should be deleted, but does not trigger save changes in the context
+        ''' </summary>
+        ''' <param name="db"></param>
+        ''' <remarks>This should be used when you want to delete an item but not trigger an additional save changes, the protected handle functions should only use this for other objects</remarks>
         Public Sub DeletePartial(ByVal db As DbContext)
             If db IsNot Nothing Then
                 db.ChangeTracker.DetectChanges()
@@ -143,7 +162,11 @@ Namespace CodeFirst
                 End Try
             End If
         End Sub
-
+        ''' <summary>
+        ''' Marks that this item should be inserted/updated, but does not trigger SaveChanges on context
+        ''' </summary>
+        ''' <param name="db"></param>
+        ''' <remarks>This should be used when you want to insert an item or trigger an item should be saved, the protected handle function should only use this</remarks>
         Public Sub SavePartial(ByVal db As DbContext)
             If db IsNot Nothing Then
                 db.ChangeTracker.DetectChanges()
@@ -167,7 +190,11 @@ Namespace CodeFirst
                 End If
             End If
         End Sub
-
+        ''' <summary>
+        ''' Marks item should be saved, and triggers SaveCanges to submit all changes on the current context
+        ''' </summary>
+        ''' <param name="db"></param>
+        ''' <remarks>This should not be used from inside the protected handle functions</remarks>
         Public Sub Save(ByVal db As DbContext)
             If db IsNot Nothing Then
                 CType(db, Object).HAS.clear()
@@ -187,13 +214,23 @@ Namespace CodeFirst
                 'If Not Me.IsValid Then Throw New Exception("Rule violations prevent saving")
             End If
         End Sub
+        ''' <summary>
+        ''' Marks an item by it's id should be deleted then triggers all changes on the context should be done to the database
+        ''' </summary>
+        ''' <param name="db"></param>
+        ''' <param name="id">Id of item to delete</param>
+        ''' <remarks>This should not be used from inside the protected handle functions. also this triggers a load from the db first</remarks>
         Public Shared Sub Delete(ByVal db As DbContext, id As Integer)
             Dim item = Convert(Load(db, id, True))
             If item IsNot Nothing Then
                 item.Delete(db)
             End If
         End Sub
-
+        ''' <summary>
+        ''' Marks an item should be deleted then triggers all changes on the context should be done to the database
+        ''' </summary>
+        ''' <param name="db"></param>
+        ''' <remarks>This should not be used from inside the protected handle functions</remarks>
         Public Sub Delete(ByVal db As DbContext)
             If db IsNot Nothing Then
                 'Try
