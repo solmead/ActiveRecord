22c6a8a1c21c5996744001814ad7c6407e9bf601
Chris Powell <powelcr@ucmail.uc.edu>
2013-09-13 17:49:01 -0400

    Switch to composition methodology

---
+++ ActiveRecord/CodeFirst/ContextExtension.vb
@@ -0,0 +1,85 @@
+﻿
+Imports System.Data.Entity
+Imports System.Runtime.InteropServices
+Imports System.CodeDom
+Imports System.Text
+
+Namespace CodeFirst
+    Public Class ContextExtension(Of TT As {IContext(Of TT), DbContext})
+
+        Private bContext As TT
+
+        Public Sub New(baseContext As TT)
+            baseContext.Extension = Me
+            bContext = baseContext
+        End Sub
+
+
+        Private Function CheckChanges(savedObjs As List(Of Object), deletedObjs As List(Of Object)) As Boolean
+            Dim changed = False
+            bContext.ChangeTracker.DetectChanges()
+            Dim changedList = bContext.ChangeTracker.Entries().ToList
+            For Each EntityEntry In changedList
+                Dim item As Object = TryCast(EntityEntry.Entity, IRecord)
+                If item IsNot Nothing Then
+                    If (EntityEntry.State = EntityState.Added OrElse EntityEntry.State = EntityState.Modified) AndAlso Not savedObjs.Contains(item) Then
+                        item.HandleSaveBeforeEvent(Me)
+                        savedObjs.Add(item)
+                        changed = True
+                    ElseIf EntityEntry.State = EntityState.Deleted AndAlso Not deletedObjs.Contains(item) Then
+                        EntityEntry.State = EntityState.Modified
+                        For Each op In EntityEntry.OriginalValues.PropertyNames
+                            Dim ir = CType(item, IRecord)
+                            ir.SetValue(op, EntityEntry.OriginalValues(op))
+                        Next
+                        bContext.ChangeTracker.DetectChanges()
+                        item.HandleDeleteBeforeEvent(Me)
+                        EntityEntry.State = EntityState.Deleted
+                        deletedObjs.Add(item)
+                        changed = True
+                    End If
+                End If
+            Next
+            Return changed
+        End Function
+
+
+        Public Function SaveChanges() As Integer
+            bContext.ChangeTracker.DetectChanges()
+            Dim I As Integer
+            Dim savedObjs As New List(Of Object)
+            Dim deletedObjs As New List(Of Object)
+
+            While CheckChanges(savedObjs, deletedObjs)
+
+            End While
+            Try
+                'Logger.GlobalLog.DebugMessage("SaveChanges: Saving changes count=" & savedObjs.Count)
+                I = bContext.MyBaseSaveChanges()
+                'Logger.GlobalLog.DebugMessage("SaveChanges: Saved changes")
+            Catch ex As Exception
+                Dim SB As New StringBuilder()
+                'Logger.GlobalLog.DebugMessage("SaveChanges: Error")
+                For Each e In bContext.GetValidationErrors()
+                    SB.AppendLine(e.Entry.Entity.GetType().ToString & " failed validation")
+                    Debug.WriteLine(e.Entry.Entity.GetType().ToString & " failed validation")
+                    For Each se In e.ValidationErrors
+                        SB.AppendLine("     [" & se.PropertyName & "] - [" & se.ErrorMessage & "]")
+                        Debug.WriteLine("     [" & se.PropertyName & "] - [" & se.ErrorMessage & "]")
+                    Next
+                Next
+                'Dim i2 As Integer = 1
+
+                Throw New Exception("Save Changes Error, See inner exception - " & SB.ToString, ex)
+            End Try
+            For Each item In savedObjs
+                item.HandleSaveAfterEvent(Me)
+            Next
+            For Each item In deletedObjs
+                item.HandleDeleteAfterEvent(Me)
+            Next
+
+            Return I
+        End Function
+    End Class
+End Namespace
---
+++ ActiveRecord/CodeFirst/TestContext.vb
@@ -0,0 +1,30 @@
+﻿
+Imports System.Data.Entity
+Imports Microsoft.VisualBasic.CompilerServices
+
+Namespace CodeFirst
+    Public Class TestContext
+        Inherits DbContext
+        Implements IContext(Of TestContext)
+
+        Public Sub New()
+            MyBase.New()
+            Extension = New ContextExtension(Of TestContext)(Me)
+        End Sub
+        Public Sub New(nameOrConnectionString As String)
+            MyBase.New(nameOrConnectionString)
+            Extension = New ContextExtension(Of TestContext)(Me)
+        End Sub
+
+        Public Property Extension As ContextExtension(Of TestContext) Implements IContext(Of TestContext).Extension
+
+        Public Function MyBaseSaveChanges() As Integer Implements IContext(Of TestContext).MyBaseSaveChanges
+            Return MyBase.SaveChanges()
+        End Function
+
+        Public Overrides Function SaveChanges() As Integer
+            Return Extension.SaveChanges()
+        End Function
+
+    End Class
+End Namespace
